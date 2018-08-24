d8068c3576c0710f68f9651477b5d500f5af04f7
Guilherme Jordao <gui.jordao.1994@gmail.com>
2018-08-24 15:07:24 -0300

    Update convertCsvToJson.py
    sample file names

--- convertCsvToJson.py
+++ convertCsvToJson.py
@@ -2,8 +2,8 @@ import csv
 import json
 import codecs
 
-csvfile = codecs.open('fundos_temp.csv', 'r', encoding="utf8")
-jsonfile = codecs.open('fundos_temp.json', 'w', encoding="utf8")
+csvfile = codecs.open('file.csv', 'r', encoding="utf8")
+jsonfile = codecs.open('file.json', 'w', encoding="utf8")
 
 fieldnames = ('''here you put the field names of your JSON file''')
 reader = csv.DictReader( csvfile, fieldnames)
